87896c6a28fcebbf379e85248ef7809d0622d431
Mawsis <Mawsis448@outlook.com>
2023-12-15 22:51:34 +0100

    feat: Dashboard components complete

--- confia-react/src/pages/Dashboard.jsx
+++ confia-react/src/pages/Dashboard.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import {User} from 'lucide-react'
+import {User,BookOpenText,UtensilsCrossed,Bath,Settings} from 'lucide-react'
 
 export default function Dashboard() {
   return (
-    <div className="grid grid-cols-7 grid-rows-9 gap-4 h-screen w-screen p-5">
-        <div className="col-span-2 row-span-1 h-full w-full bg-[#FFF2DF] rounded-xl flex items-center">
-            <img className='ml-16' src="./src/assets/Group.svg" alt=""/>
+    <div className="grid grid-cols-6 gap-4 h-screen w-screen p-5">
+        <div className="col-span-1 row-span-1 h-full w-full bg-[#FFF2DF] rounded-xl flex items-center justify-center">
+            <img  src="./src/assets/Group.svg" alt=""/>
         </div>
 
         <div className="col-span-5 row-span-1 h-full w-full flex bg-[#FFF2DF] rounded-xl px-10 justify-between">
@@ -29,9 +29,46 @@ export default function Dashboard() {
             </div>
         </div>
 
-        <div className="col-span-2 row-span-6 h-full w-full bg-[#FFF2DF] rounded-xl">3</div>
+        <div className="col-span-1 row-[span_7_/_span_7] h-full w-full bg-[#FFF2DF] rounded-xl flex flex-col justify-between">
+            <div className=' flex flex-col space-y-6 justify-start pt-7'>
+                <div className='flex pl-11 items-center space-x-5 text-[#F29E58] font-bold text-xl'>
+                    <div>
+                        <BookOpenText />
+                    </div>
+                    <div>
+                        Booking
+                    </div>
+                </div>
+                <div className='flex pl-11 items-center space-x-5 text-[#939393] font-bold text-xl'>
+                    <div>
+                        <UtensilsCrossed />
+                    </div>
+                    <div>
+                        Restaurant
+                    </div>
+                </div>
+                <div className='flex pl-11 items-center space-x-5 text-[#939393] font-bold text-xl'>
+                    <div>
+                        <Bath />
+                    </div>
+                    <div>
+                        SPA
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div className='flex pl-11 items-center space-x-5 text-[#939393] font-bold text-xl pb-7'>
+                    <div>
+                        <Settings />
+                    </div>
+                    <div>
+                        Settings
+                    </div>
+                </div>
+            </div>
+        </div>
 
-        <div className="col-span-5 row-span-6 h-full w-full bg-[#FFF2DF] rounded-xl">4</div>
+        <div className="col-span-5 row-[span_7_/_span_7] h-full w-full bg-[#F7F7F7] rounded-2xl">4</div>
     </div>
   )
 }
